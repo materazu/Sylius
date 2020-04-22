a2eae8b96f18382b30e68e2b105730ac0fcf644c
Kamil Kokot <kamil@kokot.me>
2020-04-22 11:54:33 +0200

    Switch to Doctrine/Migrations 3.0-alpha

---
+++ src/Sylius/Bundle/CoreBundle/Migrations/ProjectVersionFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Migrations;
+
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\Migrations\Version\MigrationFactory;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+final class ProjectVersionFactory implements MigrationFactory
+{
+    private $container;
+    private $migrationFactory;
+
+    public function __construct(MigrationFactory $migrationFactory, ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->migrationFactory = $migrationFactory;
+    }
+
+    public function createVersion(string $migrationClassName): AbstractMigration
+    {
+        $instance = $this->migrationFactory->createVersion($migrationClassName);
+
+        if ($instance instanceof ContainerAwareInterface) {
+            $instance->setContainer($this->container);
+        }
+
+        return $instance;
+    }
+}
